bebf45362941352f11cf55dfb43d6876e3ec1fae
Nikolina Shopova <nikolina.shopova@gmail.com>
2016-12-07 15:28:31 +0100

    change shuffle and sorting functions, complete styling

--- js/script.js
+++ js/script.js
@@ -1,37 +1,26 @@
-$(document).ready(function(){
-    console.log('hello');
+$(document).ready(function () {
+    $('.buttons-container').on('click', '.shuffle', shuffleTiles);
+    $('.buttons-container').on('click', '.sort', sortTiles);
 
-$('.buttons-container').on('click', '.shuffle', shuffleTiles);
+    var tilesContainer = $('.tiles-container');
 
-$('.buttons-container').on('click', '.sort', sortTiles);
-
-
-});
-var tilesContainer = $('.tiles-container');
-var tiles = tilesContainer.children();
-var tilesToShuffle = tiles;
-
-
-function shuffleTiles(){
-    tilesContainer.empty();
-
-        while(tilesToShuffle.length)
-        {
+    function shuffleTiles() {
+        var tiles = tilesContainer.children();
+        tilesContainer.empty();
+        var tilesToShuffle = tiles;
+        while (tilesToShuffle.length) {
             tilesContainer.append(tilesToShuffle.splice(Math.floor(Math.random() * tilesToShuffle.length), 1)[0]);
         }
-        tilesToShuffle=tilesContainer.children();
-        return false;
-}
+    }
 
-function sortTiles(){
-    tiles=tilesContainer.children();
-    tiles.sort(function(a, b){
-        var ac =parseInt( a.innerText,10);
-        var bc =parseInt( b.innerText,10);
-      return ac-bc});
+    function sortTiles() {
+        var tiles = tilesContainer.children();
+        tiles.sort(function (a, b) {
+            var ac = parseInt(a.innerText, 10);
+            var bc = parseInt(b.innerText, 10);
+            return ac - bc
+        });
 
-    tilesContainer.html(tiles);
-
-    return false;
-}
-
+        tilesContainer.html(tiles);
+    }
+});
